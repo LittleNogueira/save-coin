f2a264c1efd437e562b2ed263a7328600be3af2d
Mateus Nogueira <mateus_nogol@outlook.com>
2022-11-16 20:25:50 -0300

    feat: remove codigo de insercao em tabelas local

--- app/src/main/java/com/example/save_coin/AdicionarBalancoActivity.kt
+++ app/src/main/java/com/example/save_coin/AdicionarBalancoActivity.kt
@@ -25,14 +25,13 @@ import retrofit2.Response
 import java.math.BigDecimal
 
 class AdicionarBalancoActivity : AppCompatActivity() {
-    @RequiresApi(Build.VERSION_CODES.O)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adicionar_balanco)
 
         val intent = Intent(this, DashboardActivity::class.java)
         val sharedPreference = UsuarioSharedPreference(this)
-        val databaseManager = BalancoDatabaseManager(this)
         val saveCoinClient = SaveCoinClient()
 
         val usuarioPreference = sharedPreference.get()
@@ -52,29 +51,11 @@ class AdicionarBalancoActivity : AppCompatActivity() {
                     Toast.makeText(applicationContext, "Houve um falha na conexao com a API!", Toast.LENGTH_LONG).show()
                 }
                 override fun onResponse(call: Call<LancamentoResponse>, response: Response<LancamentoResponse>) {
-                    val balanco = convertInputsToBalanco()
-                    databaseManager.inserir(balanco)
-
                     startActivity(intent)
                     finish()
                 }
             })
         }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun convertInputsToBalanco(): Balanco{
-        val editTextNome = findViewById<EditText>(R.id.edt_nome_balanco)
-        val editTextValor = findViewById<EditText>(R.id.edt_valor)
-        val spinnerRecorrencia = findViewById<Spinner>(R.id.spn_recorrencia)
-        val spinnerTipo = findViewById<Spinner>(R.id.spn_tipo)
-
-        return Balanco(
-            nome = editTextNome.text.toString(),
-            valor = BigDecimal(editTextValor.text.toString()),
-            tipo = Tipo.valueOf(spinnerTipo.selectedItem.toString().uppercase()),
-            recorrencia = Recorrencia.valueOf(spinnerRecorrencia.selectedItem.toString().uppercase())
-        )
     }
 
     private fun convertInputsToLancamento(): LancamentoRequest{
