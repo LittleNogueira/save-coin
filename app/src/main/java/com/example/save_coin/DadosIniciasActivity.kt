7ad2a0ac2ca74191c54a05dd793329d1e21b6787
Mateus Nogueira <mateus_nogol@outlook.com>
2022-11-16 21:11:24 -0300

    feat: persite lancamento inicial

--- app/src/main/java/com/example/save_coin/DadosIniciasActivity.kt
+++ app/src/main/java/com/example/save_coin/DadosIniciasActivity.kt
@@ -7,13 +7,18 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.save_coin.client.SaveCoinClient
+import com.example.save_coin.client.request.LancamentoRequest
 import com.example.save_coin.client.request.UsuarioRequest
+import com.example.save_coin.client.response.LancamentoResponse
 import com.example.save_coin.client.response.UsuarioResponse
+import com.example.save_coin.enumeration.Recorrencia
+import com.example.save_coin.enumeration.Tipo
 import com.example.save_coin.shared_preference.UsuarioSharedPreference
 import com.example.save_coin.shared_preference.model.UsuarioPreference
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.math.BigDecimal
 
 class DadosIniciasActivity : AppCompatActivity() {
 
@@ -23,34 +28,47 @@ class DadosIniciasActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dados_inicias)
 
-        val intent = Intent(this, DashboardActivity::class.java)
-        val sharedPreference = UsuarioSharedPreference(this)
-        val saveCoinClient = SaveCoinClient()
-
         val buttonVamosEconomizar = findViewById<Button>(R.id.btn_vamos_economizar)
 
         buttonVamosEconomizar.setOnClickListener(){
-
-            buttonVamosEconomizar.isEnabled = false;
-
-            val usuarioRequest = convertInputToUsuarioRequest()
-            val callback = saveCoinClient.criarUsuario(usuarioRequest)
-
-            callback.enqueue(object : Callback<UsuarioResponse> {
-                override fun onFailure(call: Call<UsuarioResponse>, t: Throwable) {
-                    buttonVamosEconomizar.isEnabled = true;
-                    Toast.makeText(applicationContext, "Houve um falha na conexao com a API!", Toast.LENGTH_LONG).show()
-                }
-                override fun onResponse(call: Call<UsuarioResponse>, response: Response<UsuarioResponse>) {
-                    val usuarioPreference = convertInputsToUsuarioPreference(response.body()!!)
-                    sharedPreference.put(usuarioPreference)
-
-                    startActivity(intent)
-                    finish()
-                }
-            })
-
+            salvarDadosInicias()
         }
+    }
+
+    private fun salvarDadosInicias(){
+        val intent = Intent(this, DashboardActivity::class.java)
+        val buttonVamosEconomizar = findViewById<Button>(R.id.btn_vamos_economizar)
+        val sharedPreference = UsuarioSharedPreference(this)
+        val saveCoinClient = SaveCoinClient()
+
+        buttonVamosEconomizar.isEnabled = false;
+
+        val usuarioRequest = convertInputToUsuarioRequest()
+        val callback = saveCoinClient.criarUsuario(usuarioRequest)
+
+        callback.enqueue(object : Callback<UsuarioResponse> {
+            override fun onFailure(call: Call<UsuarioResponse>, t: Throwable) {
+                buttonVamosEconomizar.isEnabled = true;
+                Toast.makeText(applicationContext, "Houve um falha na conexao com a API!", Toast.LENGTH_LONG).show()
+            }
+            override fun onResponse(call: Call<UsuarioResponse>, response: Response<UsuarioResponse>) {
+                val usuarioPreference = convertInputsToUsuarioPreference(response.body()!!)
+                sharedPreference.put(usuarioPreference)
+
+                val lancamentoRequest = convertInputToLancamentoRequest()
+                val callbackLancamento = saveCoinClient.criarLancamento(lancamentoRequest, usuarioPreference!!.uuid)
+
+                callbackLancamento.enqueue(object : Callback<LancamentoResponse> {
+                    override fun onFailure(call: Call<LancamentoResponse>, t: Throwable) {
+                        Toast.makeText(applicationContext, "Houve um falha na conexao com a API!", Toast.LENGTH_LONG).show()
+                    }
+                    override fun onResponse(call: Call<LancamentoResponse>, response: Response<LancamentoResponse>) {
+                        startActivity(intent)
+                        finish()
+                    }
+                })
+            }
+        })
     }
 
     private fun convertInputsToUsuarioPreference(usuarioResponse: UsuarioResponse): UsuarioPreference {
@@ -58,6 +76,17 @@ class DadosIniciasActivity : AppCompatActivity() {
             uuid = usuarioResponse.uuid,
             nome = usuarioResponse.nome,
             email = usuarioResponse.email
+        )
+    }
+
+    private fun convertInputToLancamentoRequest(): LancamentoRequest {
+        val editTextReceita = findViewById<TextView>(R.id.edt_receita_liquida_mensal)
+
+        return LancamentoRequest(
+            nome = BALANCO_INICIAL,
+            valor = BigDecimal(editTextReceita.text.toString()),
+            tipo = Tipo.ENTRADA,
+            recorrencia = Recorrencia.MENSAL
         )
     }
 
